e62b89ec402f7f072ba9c9353ea242afa3818a8d
Mikhail A <mikhail@landofcash.net>
2025-09-30 13:35:16 +0100

    ver bump to 0.2.4

--- aptoosh-seller/src/config.ts
+++ aptoosh-seller/src/config.ts
@@ -1,6 +1,7 @@
 import type {NetworkId} from "@/context/wallet/types.ts";
 
-export const APP_VERSION = '0.2.3'
+declare const __APP_VERSION__: string;
+export const APP_VERSION = __APP_VERSION__
 export const APP_NAME='Aptoosh'
 export const BASE_URL='https://aptoosh.com'
 
